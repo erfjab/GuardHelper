48d66f80b95623df469c540557cc249af110b334
Erfan <143827987+erfjab@users.noreply.github.com>
2026-01-28 21:56:05 +0330

    feat: api base with fiber (#3)

---
+++ internal/routes/users/route.go
@@ -0,0 +1,12 @@
+package users
+
+import (
+	"guardhelper/internal/middlewares"
+	"github.com/gofiber/fiber/v2"
+)
+
+func RegisterUserRoutes(app *fiber.App) {
+	userGroup := app.Group("/api/users") 
+
+	userGroup.Get("/", middlewares.AuthApiKey, GetAllUsers)
+}
--- main.go
+++ main.go
@@ -2,16 +2,31 @@ package main
 
 import (
 	"guardhelper/internal/config"
+	usersRoutes "guardhelper/internal/routes/users"
 	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func main() {
-
-	cfg, err := config.LoadConfig()
+	log.Printf("Starting GuardHelper...")
+	_, err := config.LoadConfig()
 	if err != nil {
 		log.Fatalf("Failed to load config: %v", err)
 	}
-
-	log.Printf("Config loaded successfully: %+v", cfg.ApiKey)
-
+	log.Printf("Config loaded successfully.")
+	fiberApp := fiber.New(fiber.Config{DisableStartupMessage: true})
+	fiberApp.Use(logger.New())
+	usersRoutes.RegisterUserRoutes(fiberApp)
+	fiberApp.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{"message": "GuardHelper is running"})
+	})
+	fiberApp.Use(func(c *fiber.Ctx) error {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"message": "not found",
+		})
+	})
+	log.Printf("Listening on http://0.0.0.0:99")
+	log.Fatal(fiberApp.Listen(":99"))
 }
